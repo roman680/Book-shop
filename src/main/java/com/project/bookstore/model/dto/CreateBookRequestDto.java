78a3b1f5a0683fc39d499dc45593467c9d4462ca
Roman Denysiuk <r.v.denysiuk@gmail.com>
2024-01-06 15:19:31 +0200

    added @Valid and GlobalExceptionHandler

--- src/main/java/com/project/bookstore/model/dto/CreateBookRequestDto.java
+++ src/main/java/com/project/bookstore/model/dto/CreateBookRequestDto.java
@@ -1,13 +1,22 @@
 package com.project.bookstore.model.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
+    @ISBN
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
