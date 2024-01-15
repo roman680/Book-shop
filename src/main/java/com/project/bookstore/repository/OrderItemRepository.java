ccafc9eaacca8e4b27ff7d90ea5271d565ed0c82
Roman Denysiuk <r.v.denysiuk@gmail.com>
2024-01-15 21:27:52 +0200

    final fixes

--- src/main/java/com/project/bookstore/repository/OrderItemRepository.java
+++ src/main/java/com/project/bookstore/repository/OrderItemRepository.java
@@ -4,9 +4,7 @@ import com.project.bookstore.model.OrderItem;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
     @Query("""
--- src/main/java/com/project/bookstore/repository/OrderRepository.java
+++ src/main/java/com/project/bookstore/repository/OrderRepository.java
@@ -4,9 +4,7 @@ import com.project.bookstore.model.Order;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("""
