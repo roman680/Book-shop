cd34cff048c40e9cade37a7947011a7b144fa3f0
Roman Denysiuk <r.v.denysiuk@gmail.com>
2024-01-23 20:43:07 +0200

    some changes

--- src/main/java/com/project/bookstore/model/Book.java
+++ src/main/java/com/project/bookstore/model/Book.java
@@ -15,38 +15,33 @@ import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
-@Data
 @Entity
+@Data
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "books")
 public class Book {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(nullable = false)
     private String title;
-
     @Column(nullable = false)
     private String author;
-
-    @Column(nullable = false, unique = true)
+    @Column(unique = true, nullable = false)
     private String isbn;
-
     @Column(nullable = false)
     private BigDecimal price;
-
-    @Column(nullable = false)
     private String description;
-
-    @Column(nullable = false)
     private String coverImage;
-
-    @Column(columnDefinition = "false")
-    private boolean isDeleted;
-
+    @Column(nullable = false)
+    private boolean isDeleted = false;
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
     @ManyToMany(fetch = FetchType.EAGER)
--- src/main/java/com/project/bookstore/model/Category.java
+++ src/main/java/com/project/bookstore/model/Category.java
@@ -7,9 +7,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
-@Data
 @Entity
+@Data
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "categories")
 public class Category {
     @Id
@@ -21,4 +27,8 @@ public class Category {
     private String description;
     @Column(nullable = false)
     private boolean isDeleted = false;
+
+    public Category(Long id) {
+        this.id = id;
+    }
 }
